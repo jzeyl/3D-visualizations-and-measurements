335c5f39a7c0c4fdd2f611c929b287206b641c22
jzeyl <jeffzeyl@gmail.com>
2019-08-11 14:54:27 +0200

    Update draw circle and umbo and plot.R

--- draw circle and umbo and plot.R
+++ draw circle and umbo and plot.R
@@ -14,8 +14,6 @@ umbo<-as.data.frame(cbind(x,y,z))
 withumbo<-data.frame(rbind(circle3d,umbo))
 
 # plot --------------------------------------------------------------------
-plot(circlex,circley)#2d
-
 library("scatterplot3d") # load. #3d
 plot3d <- scatterplot3d(circle3d, angle = 70, pch = 16)
 plot3dwith_umbo <- scatterplot3d(withumbo, angle = 70, pch = 16)
